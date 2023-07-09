77a4cc0e9611418d13b0fabfd304e9d28f770323
kyrbrbik <103336573+kyrbrbik@users.noreply.github.com>
2023-07-09 07:22:25 +0200

    changed to lua

---
+++ init.lua
@@ -0,0 +1,40 @@
+vim.cmd([[
+set number
+set autoindent
+set tabstop=4
+set shiftwidth=4
+set smarttab
+set softtabstop=4
+set mouse=a
+set relativenumber
+call plug#begin()
+
+Plug 'arcticicestudio/nord-vim'
+Plug 'https://github.com/vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-fugitive'
+Plug 'nvim-treesitter/nvim-treesitter'
+Plug 'nvim-treesitter/nvim-treesitter-context'
+Plug 'neovim/nvim-lspconfig'
+Plug 'VonHeikemen/lsp-zero.nvim', { 'branch': 'v2.x' }
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+
+
+call plug#end()
+
+let g:airline_theme='base16_nord'
+let g:copilot_filetypes = {
+			\ 'yaml': v:true,
+			\ }
+]])
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup()
