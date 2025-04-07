11e24bbac9978db6d6751116eef9e676061e273c
kyrbrbik <>
2025-04-07 17:16:40 +0200

    fix autocomplete

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,19 +23,21 @@ return {
       local cmp = require('cmp')
 
       cmp.setup({
-        sources = {
-          {name = 'nvim_lsp'},
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        }),
+
         snippet = {
           expand = function(args)
             vim.snippet.expand(args.body)
           end,
         },
+        sources = {
+          {name = 'nvim_lsp'},
+        },
+        mapping = cmp.mapping.preset.insert({
+		  ['<C-Space>'] = cmp.mapping.complete(),
+		  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        }),
       })
     end
   },
