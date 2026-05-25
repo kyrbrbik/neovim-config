beca2d2febe5d9eb8d935de8d22ccdccc641c2a9
Jan Breitkopf <breitbrbik@gmail.com>
2026-05-25 10:27:21 +0200

    chore: update treesitter to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,48 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-    branch = "master",
+	branch = "main",
 	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
+	lazy = false,
 	dependencies = {
-		"windwp/nvim-ts-autotag",
+		{
+			"windwp/nvim-ts-autotag",
+			config = function()
+				require("nvim-ts-autotag").setup()
+			end,
+		},
 	},
 
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-		treesitter.setup({
-			highlight = {
-				enable = true,
-			},
+		local parsers = {
+			"json",
+			"yaml",
+			"markdown",
+			"markdown_inline",
+			"lua",
+			"vim",
+			"vimdoc",
+			"go",
+			"powershell",
+			"bash",
+			"javascript",
+			"typescript",
+			"tsx",
+			"python",
+			"query",
+		}
 
-			ensure_installed = {
-				"json",
-				"yaml",
-				"markdown",
-				"lua",
-				"vim",
-				"go",
-				"powershell",
-				"bash",
-				"javascript",
-				"typescript",
-			},
+		require("nvim-treesitter").install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local buf = args.buf
+				local ft = vim.bo[buf].filetype
+				local lang = vim.treesitter.language.get_lang(ft) or ft
+				if not pcall(vim.treesitter.start, buf, lang) then
+					return
+				end
+				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
